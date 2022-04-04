411fcdcac94849e4c64671cb89d44f72f25b8674
ddanilooalves <daniloalves0512@gmail.com>
2022-04-03 22:14:33 -0300

    ATUALIZAÇÃO POSTGRES

--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,7 @@
 const Sequelize = require('sequelize');
 const connect = new Sequelize(
-    process.env.DB_BASE,
-    process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DATABASE_URL,
     {
-       host: process.env.DB_HOST,
-       port: 5432,
        dialect: "postgres",
     }
 );
